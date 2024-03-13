badf2382060c31dfdddefcba7faf32f558262f39
takadr <t4k4.cr@gmail.com>
2024-03-13 10:27:10 +0900

    fix link to the explanation for templates

--- packages/ui/components/auctions/AuctionFormWrapper.tsx
+++ packages/ui/components/auctions/AuctionFormWrapper.tsx
@@ -33,7 +33,7 @@ export default function AuctionFormWrapper(props: AuctionFormWrapperParams) {
           <QuestionIcon mb={1} ml={1} />
         </Tooltip>
         <Link
-          href="https://docs.yamawake.xyz/shi-yang-shui-ming/template"
+          href="https://docs.yamawake.xyz/shi-yang-shuo-ming/template/templatev1"
           target="_blank"
           float={"right"}
           ml={4}
