d9ed2d08b382fd7565bce2dfb6d169a1f8f8bd38
takadr <t4k4.cr@gmail.com>
2024-04-05 11:52:05 +0900

    remove unused constants

--- packages/lib/constants/templates.ts
+++ packages/lib/constants/templates.ts
@@ -13,13 +13,3 @@ export const COMPATIBLE_TEMPLATES: CompatibleTemplates = {
   [sepolia.id]: [TEMPLATE_V1_NAME],
   [hardhat.id]: [TEMPLATE_V1_NAME],
 };
-
-export const LOCK_DURATION: { [key: string]: number } = {
-  // TemplateV1 -> 3 day
-  [TEMPLATE_V1_NAME]: 86400 * 3,
-};
-
-export const FEE_RATE_PER_MIL: { [key: string]: number } = {
-  // TemplateV1
-  [TEMPLATE_V1_NAME]: 1,
-};
