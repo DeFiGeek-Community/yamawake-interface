e8942e1315ff6b9e59e168389a9eb385df37e8e6
takadr <t4k4.cr@gmail.com>
2024-07-10 11:13:49 +0900

    add ccip related type definitions

--- packages/lib/constants/ccip.ts
+++ packages/lib/constants/ccip.ts
@@ -4,3 +4,34 @@ export enum CCIP_MESSAGE_STATES {
   SUCCESS,
   FAILURE,
 }
+
+export type FeeToken = {
+  symbol: string;
+  address: `0x${string}`;
+};
+
+export type EVMTokenAmount = {
+  token: string;
+  amount: bigint;
+};
+
+export type CCIPSendRequestedEventArgs = {
+  sourceChainSelector: bigint;
+  sender: string;
+  receiver: string;
+  sequenceNumber: bigint;
+  gasLimit: bigint;
+  strict: boolean;
+  nonce: bigint;
+  feeToken: string;
+  feeTokenAmount: bigint;
+  data: string;
+  tokenAmounts: EVMTokenAmount[];
+  sourceTokenData: string[];
+  messageId: string;
+};
+
+export type DecodedLog<T> = {
+  eventName: string;
+  args: T;
+};
