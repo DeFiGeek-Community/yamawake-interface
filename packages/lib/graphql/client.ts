177472a6b16dc1ef051a597d6c7047fdd93696e8
takadr <t4k4.cr@gmail.com>
2024-07-19 12:14:05 +0900

    add debug code for an inspection

--- packages/lib/graphql/client.ts
+++ packages/lib/graphql/client.ts
@@ -1,5 +1,5 @@
-import { ClientError, GraphQLClient } from "graphql-request";
-import type { RequestConfig, ResponseMiddleware } from "graphql-request/build/esm/types";
+import { GraphQLClient } from "graphql-request";
+import type { RequestConfig } from "graphql-request/build/esm/types";
 import { Chain } from "viem/chains";
 import { SUBGRAPH_ENDPOINTS, SUBGRAPH_SECONDARY_ENDPOINTS } from "../constants/subgraphEndpoints";
 import { getDefaultChain, getSupportedChain } from "../utils/chain";
@@ -22,11 +22,24 @@ export class GraphQLChainClient extends GraphQLClient {
     if (typeof chainId !== "undefined") {
       requestedChain = getSupportedChain(chainId);
     }
+
     if (requestedChain)
       endpoint = useSecondaryEndpoint
         ? SUBGRAPH_SECONDARY_ENDPOINTS[requestedChain.id]
         : SUBGRAPH_ENDPOINTS[requestedChain.id];
-    if (typeof endpoint === "undefined") throw Error("Subgraph endpoint is not set correctly");
+
+    console.log("---");
+    console.log(endpoint);
+    console.log(
+      useSecondaryEndpoint,
+      getDefaultChain().id,
+      requestedChain,
+      SUBGRAPH_SECONDARY_ENDPOINTS[getDefaultChain().id],
+      SUBGRAPH_ENDPOINTS[getDefaultChain().id],
+    );
+    console.log("---");
+
+    if (typeof endpoint === "undefined") throw new Error("Subgraph endpoint is not set correctly");
     super(endpoint, requestConfig);
   }
 }
